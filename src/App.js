95234c23c830e53ffb407ff577bf024f101961f6
louay.baccary <louay.baccary@esprit.tn>
2020-12-22 14:12:24 +0100

    modal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import logo from "./NFTLogo.jpg";
 import "./App.css";
-import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
-import MediaCard from "./mycard";
-import WalletChoice from "./components/walletChoice";
 import Button from "@material-ui/core/Button";
-
+import Modal from "react-modal";
+import React from "react";
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "#282c34"
+  },
+};
+// Modal.setAppElement("#yourAppElement");
 function App() {
-  const contentStyle = { background: "#000" };
-  const overlayStyle = { background: "rgba(0,0,0,0.5)" };
+  var subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  // function afterOpenModal() {
+  //   // references are now sync'd and can be accessed.
+  //   subtitle.style.color = "#f00";
+  // }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -17,55 +39,71 @@ function App() {
           The simpliest and safest place to Swap or trade your NFTs without Fees
           !
         </p>
-        <Popup
-          trigger={
-            <a className="App-link" target="_blank" rel="noopener noreferrer">
-              Connect to a wallet
-            </a>
-          }
-          position="center center"
+        <a
+          className="App-link"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={() => {
+            openModal();
+          }}
         >
-             <div style={{ display: "flex", flexDirection: "row" }}>
-            <div
-              style={{
-                borderWidth: 10,
-                borderColor: "white",
-              }}
-            >
-              <h5>Metamask</h5>
+          Connect to a wallet
+        </a>
+        <Modal
+          isOpen={modalIsOpen}
+          //onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <div style={{ display: "flex", flexDirection: "row" }}>
               <div
                 style={{
-                  borderColor: "black",
-                  borderRadius: "12px",
                   borderWidth: 10,
+                  borderColor: "white",
                 }}
               >
-                <Button size="small" color="primary">
-                  Connect
-                </Button>
+                <h5>Metamask</h5>
+                <div
+                  style={{
+                    borderColor: "black",
+                    borderRadius: "12px",
+                    borderWidth: 10,
+                  }}
+                >
+                  <Button size="small" color="primary">
+                    Connect
+                  </Button>
+                </div>
               </div>
-            </div>
-            <div
-              style={{
-                borderWidth: 10,
-                borderColor: "white",
-              }}
-            >
-              <h5>WalletConnect</h5>
               <div
                 style={{
-                  borderColor: "black",
-                  borderRadius: "12px",
                   borderWidth: 10,
+                  borderColor: "white",
                 }}
               >
-                <Button size="small" color="primary">
-                  Connect
-                </Button>
+                <h5>WalletConnect</h5>
+                <div
+                  style={{
+                    borderColor: "black",
+                    borderRadius: "12px",
+                    borderWidth: 10,
+                  }}
+                >
+                  <Button size="small" color="primary" >
+                    Connect
+                  </Button>
+                </div>
               </div>
             </div>
+            <Button size="small" color="secondary" onClick={()=>{
+              closeModal()
+            }}>
+              Cancel
+            </Button>
           </div>
-        </Popup>
+        </Modal>
       </header>
     </div>
   );
